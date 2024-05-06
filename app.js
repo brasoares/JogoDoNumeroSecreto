249860c6021e15837acc0d3f80a7ca3e7ce509b5
Henoc <henocsoares@hotmail.com.br>
2024-05-06 00:43:43 -0300

    Coding

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert("Seja bem-vindo(a) ao Jogo do Número Secreto!")
-let numeroSecreto = 72;
-let chute = prompt("Escolha um valor de 0 a 9: ")
+let numeroSecreto = 7;
+let chute = prompt("Escolha um valor de 0 a 9: ");
 
 if (numeroSecreto == chute){
-    
+    console.log("Parabéns! Você acertou! O número secreto é o 7!")
 }
