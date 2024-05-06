c7101a332565e00e06e95c5175dcf37a7fe6992a
Henoc <henocsoares@hotmail.com.br>
2024-05-06 00:41:00 -0300

    Choice!

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert("Seja bem-vindo(a) ao Jogo do Número Secreto!")
 let numeroSecreto = 72;
-prompt("Escolha um valor de 0 a 9: ")
+let chute = prompt("Escolha um valor de 0 a 9: ")
+
+if (numeroSecreto == chute){
+    
+}
