bbfe0af7d9c8e64a22cc68edf6b62bd0cd2501df
Henoc <henocsoares@hotmail.com.br>
2024-05-08 01:48:49 -0300

    Prompting & more

--- Exercises/HoraDaPratica/ageRequest.js
+++ Exercises/HoraDaPratica/ageRequest.js
@@ -1,2 +1,6 @@
 // Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
 let idade = prompt('Digite a sua idade: ');
+
+if (idade >= 18) {
+  alert('Pode tirar a habilitação!');
+}
