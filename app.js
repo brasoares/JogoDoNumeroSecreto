8ffa8aacbacc6af2fed7d33c9b2c14948579ed60
Henoc <henocsoares@hotmail.com.br>
2024-05-09 04:20:15 -0300

    else

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ let chute = prompt("Escolha um valor de 0 a 9: ");
 if (chute == numeroSecreto){
     alert("Parabéns! Você acertou! O número secreto é o 7!");
 } else {
-    
+    alert("Errrrrrrrrrrrrrou!");
 }
