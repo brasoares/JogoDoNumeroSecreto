2bc9d5475f484688a40fab1c39b1b1c5f80a10bc
Henoc <henocsoares@hotmail.com.br>
2024-05-10 23:32:21 -0300

    Template Strings

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let chute = prompt("Escolha um valor de 0 a 9: ");
 } */
 
 if (chute == numeroSecreto){
-    alert("Parabéns! Você acertou! O número secreto é o 7!");
+    alert(`Parabéns! Você acertou! O número secreto é o 7!`);
 } else {
     alert("Errrrrrrrrrrrrrou!");
 }
