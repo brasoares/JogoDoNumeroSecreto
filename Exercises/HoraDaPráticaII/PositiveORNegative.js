42b73606160935eb2575139153879a0a668f37cb
Henoc <henocsoares@hotmail.com.br>
2024-05-13 23:38:55 -0300

    Hora da Prática II!

--- Exercises/HoraDaPráticaII/PositiveORNegative.js
+++ Exercises/HoraDaPráticaII/PositiveORNegative.js
@@ -1,1 +1,9 @@
 numToCheck = prompt("Informe um número qualquer: ");
+
+if (numToCheck > 0) {
+    alert("You typed a positive number!")
+} else if () {
+    alert("You typed zero!")
+} else {
+    alert("You typed a negative number!")
+}
