226ecc25e8a8747bd9a06848c8a19985a5e5085a
brasoares <henocsoares@hotmail.com.br>
2024-05-25 22:37:14 -0300

    Updating structure and code

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 alert("Seja bem-vindo(a) ao Jogo do Número Secreto!")
 let numeroSecreto = 7;
+let chute = prompt("Escolha um valor de 0 a 9: ");
 
 // enquanto o chute não for igual ao nosso nº secreto
 
-while(chute != numeroSecreto){
-    let chute = prompt("Escolha um valor de 0 a 9: ");
+while (chute != numeroSecreto) {
+	chute = prompt("Escolha um valor de 0 a 9: ");
 
-    // se o chute for igual ao número secreto
+	// se o chute for igual ao número secreto
 
-    /* if (chute == numeroSecreto){
-    console.log("Parabéns! Você acertou! O número secreto é o 7!");
-    } */
+	/* if (chute == numeroSecreto){
+	console.log("Parabéns! Você acertou! O número secreto é o 7!");
+	} */
 
-    if (chute == numeroSecreto){
-        alert(`Parabéns! Você acertou! O número secreto é o ${numeroSecreto}!`);
-    } else {
-        if(chute > numeroSecreto) {
-                alert(`O número secreto é menor que ${chute}`);
-            } else {
-                alert(`O número secreto é maior que ${chute}`);
-            }
-        }
+	if (chute == numeroSecreto) {
+		alert(`Parabéns! Você acertou! O número secreto é o ${numeroSecreto}!`);
+	} else {
+		if (chute > numeroSecreto) {
+			alert(`O número secreto é menor que ${chute}`);
+		} else {
+			alert(`O número secreto é maior que ${chute}`);
+		}
+	}
 }
