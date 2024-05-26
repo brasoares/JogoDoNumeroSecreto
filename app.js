82da12509f79bb2b470561d39fac90e690039e32
brasoares <henocsoares@hotmail.com.br>
2024-05-25 22:27:28 -0300

    Updating structure

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ alert("Seja bem-vindo(a) ao Jogo do Número Secreto!")
 let numeroSecreto = 7;
 let chute = prompt("Escolha um valor de 0 a 9: ");
 
+// enquanto o chute não for igual ao nosso nº secreto
 
+while
 
 // se o chute for igual ao número secreto
 
