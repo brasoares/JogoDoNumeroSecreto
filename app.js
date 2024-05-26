8c946e2825d7626413e38ce7987b7de596d36224
brasoares <henocsoares@hotmail.com.br>
2024-05-25 22:35:02 -0300

    Updating structure and code

--- app.js
+++ app.js
@@ -5,21 +5,20 @@ let numeroSecreto = 7;
 
 while(chute != numeroSecreto){
     let chute = prompt("Escolha um valor de 0 a 9: ");
-}
 
-// se o chute for igual ao número secreto
+    // se o chute for igual ao número secreto
 
-/* if (chute == numeroSecreto){
+    /* if (chute == numeroSecreto){
     console.log("Parabéns! Você acertou! O número secreto é o 7!");
-} */
+    } */
 
-
-if (chute == numeroSecreto){
-    alert(`Parabéns! Você acertou! O número secreto é o ${numeroSecreto}!`);
-} else {
-    if(chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
+    if (chute == numeroSecreto){
+        alert(`Parabéns! Você acertou! O número secreto é o ${numeroSecreto}!`);
+    } else {
+        if(chute > numeroSecreto) {
+                alert(`O número secreto é menor que ${chute}`);
+            } else {
+                alert(`O número secreto é maior que ${chute}`);
+            }
         }
-    }
+}
