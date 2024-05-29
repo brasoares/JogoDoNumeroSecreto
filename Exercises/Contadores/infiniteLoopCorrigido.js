1df130024c42fe4a38480bf3080b954c34430c4a
brasoares <henocsoares@hotmail.com.br>
2024-05-28 23:37:34 -0300

    Assime é

---
+++ Exercises/Contadores/infiniteLoopCorrigido.js
@@ -0,0 +1,21 @@
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média: ')
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0){
+    let numero = parseInt(prompt('Digite o número: '));
+    soma += numero;
+    contador--;
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);
+
+/* Willian está iniciando no mundo da programação e recentemente descobriu que existe uma maneira de executar um bloco de comandos repetidamente 
+enquanto uma condição pré-estabelecida não for satisfeita.
+
+Animado com as possibilidades, ele decidiu treinar a utilização da estrutura while() em um projeto pessoal, de cálculo de médias aritméticas. 
+No entanto, acabou se deparando com um loop infinito, e não conseguiu descobrir o por quê.
+
+Sabendo que você também está estudando lógica de programação, Willian pediu sua ajuda para localizar onde está o erro em seu código: */
