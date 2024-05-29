c9849a3a03bc35a36329c8fd71a5c8408947b9ad
brasoares <henocsoares@hotmail.com.br>
2024-05-28 23:31:36 -0300

    Assime é

--- Exercises/Contadores/counter.js
+++ Exercises/Contadores/counter.js
@@ -4,3 +4,10 @@ while (contador < 4) {
 	console.log('Executando a iteração ' + contador);
 	contador++;
 }
+
+/* Saber trabalhar com loops de repetição, como o while, é essencial na programação! Os loops permitem automatizar tarefas repetitivas e lidar com grandes quantidades
+de dados de forma eficiente.
+
+Contudo, é importante usá-los com cuidado para evitar loops infinitos (quando a condição nunca se torna falsa - o que pode travar o programa).
+
+É recomendado sempre ter uma lógica que faça com que a condição eventualmente se torne falsa para que o loop termine corretamente. */
