5bac3d29d2e3d8f031c5f1945d46ead3f64604d0
brasoares <henocsoares@hotmail.com.br>
2024-05-28 23:34:21 -0300

    Assime é

---
+++ Exercises/Contadores/infiniteLoop.js
@@ -0,0 +1,7 @@
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média: ')
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0){
+    let numero = parseInt(prompt('Digite o número: '));
+}
