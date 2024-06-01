7d8fe354b9b2cbca5ca118f11b321a55b72ddc9e
brasoares <henocsoares@hotmail.com.br>
2024-05-31 23:44:38 -0300

    More code!

--- Exercises/HoraDaPráticaIII/countDownToZero.js
+++ Exercises/HoraDaPráticaIII/countDownToZero.js
@@ -1,4 +1,4 @@
-/* Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número. */
+/* Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número. */
 let num = 10;
 
 while (num > 0) {
