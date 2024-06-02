b6e419dbe8c48f2e7701417ad4e6f5d40a1ec206
brasoares <henocsoares@hotmail.com.br>
2024-06-01 23:58:13 -0300

    Updating important knowledge for Clean Code and well tested maintanable code!

--- Exercises/HoraDaPráticaIII/countDownToZero.js
+++ Exercises/HoraDaPráticaIII/countDownToZero.js
@@ -4,4 +4,6 @@ let num = 10;
 while (num > 0) {
     num--;
     alert(num);
+    console.log(num); // This is also an important line of code so that a coder is able to validate via browser each interacion
+    // such validation (abovementioned) might be done without the "alert" option, in general, or depending on the situation, of course!
 }
