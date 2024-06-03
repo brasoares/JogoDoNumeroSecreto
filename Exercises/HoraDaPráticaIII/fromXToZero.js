83adecde1f5e728e27a183318c72cdd8e72ecdac
brasoares <henocsoares@hotmail.com.br>
2024-06-02 23:24:14 -0300

    HoraDaPráticaIII

--- Exercises/HoraDaPráticaIII/fromXToZero.js
+++ Exercises/HoraDaPráticaIII/fromXToZero.js
@@ -1,3 +1,7 @@
 /*Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.*/
 
 let num = prompt("Informe um número: ");
+
+while (num > 0){
+    num--;
+}
