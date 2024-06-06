ae672f7d57a2bf6ff48b45d400760be26fd6a804
brasoares <henocsoares@hotmail.com.br>
2024-06-05 23:52:57 -0300

    Getting better & better

--- Exercises/HoraDaPráticaIII/fromZeroToChosenOne.js
+++ Exercises/HoraDaPráticaIII/fromZeroToChosenOne.js
@@ -1,1 +1,5 @@
-/* */
+/* Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador. */
+
+let num = prompt("Enter a number: ")
+
+if 
