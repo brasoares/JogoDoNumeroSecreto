12c3bde102e454f33d7364c681d0477d08e7bcad
brasoares <henocsoares@hotmail.com.br>
2024-06-07 20:00:11 -0300

    Counting up!

--- Exercises/HoraDaPráticaIII/fromZeroToChosenOne.js
+++ Exercises/HoraDaPráticaIII/fromZeroToChosenOne.js
@@ -1,6 +1,7 @@
 /* Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador. */
 
-let num = prompt("Enter a number: ")
+let num = prompt("Enter a number: ");
+let counter = 0;
 
 while (counter < num) {
     
