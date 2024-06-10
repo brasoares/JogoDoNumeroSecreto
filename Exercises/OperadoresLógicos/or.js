2e331f9b95d39baa6e4215d41a61bd3a734e6f7b
brasoares <henocsoares@hotmail.com.br>
2024-06-09 23:24:26 -0300

    Operadores Lógicos

---
+++ Exercises/OperadoresLógicos/or.js
@@ -0,0 +1,9 @@
+let temMaca = false;
+let temBanana = true;
+
+// Se há maça ou há banana...
+if (temMaca || temBanana) {
+	console.log("Você tem frutas!");
+} else {
+	console.log("Não há frutas!");
+}
