f17ec9f132efa8c88f87271ee4dba37ba1e0fee4
brasoares <henocsoares@hotmail.com.br>
2024-06-09 23:20:11 -0300

    Operadores Lógicos

--- Exercises/OperadoresLógicos/and.js
+++ Exercises/OperadoresLógicos/and.js
@@ -3,5 +3,7 @@ let possuiCarteira = true;
 
 // Se idade é maior de 18 anos e possui carteira...
 if (idade > 18 && possuiCarteira){
-    console.log("Pode dirigir!")
+    console.log("Pode dirigir!");
+} else {
+    console.log("Não pode dirigir!");
 }
