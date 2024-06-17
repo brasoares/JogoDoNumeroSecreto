43b5f0bd259a0335a1db04a06290154863a19630
brasoares <henocsoares@hotmail.com.br>
2024-06-16 23:51:41 -0300

    BetterCode!

---
+++ appsOnTernaryOperator.js
@@ -0,0 +1,17 @@
+alert("Seja bem-vindo(a) ao Jogo do Número Secreto!");
+let numeroSecreto = 7;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 0;
+
+while (chute !== numeroSecreto) {
+	chute = Number(prompt("Escolha um valor de 0 a 9: "));
+	tentativas++;
+
+	if (chute === numeroSecreto) {
+		let tentativasTexto = tentativas === 1 ? "vez" : "vezes";
+		alert(`Parabéns! Você acertou! O número secreto é o ${numeroSecreto} e você tentou acertá-lo com um total de ${tentativas} ${tentativasTexto}.`)
+	} else {
+		alert(`O número secreto é ${chute > numeroSecreto ? "menor" : "maior"} que ${chute}`);
+	}
+}
