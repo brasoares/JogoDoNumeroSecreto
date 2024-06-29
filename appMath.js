bd99d79255d5b21fc6805215f5afd6d9e152ff0b
brasoares <henocsoares@hotmail.com.br>
2024-06-29 01:01:40 -0300

    Better Code!

--- appMath.js
+++ appMath.js
@@ -7,22 +7,22 @@ let chute;
 let tentativas = 0;
 
 while (chute !== numeroSecreto) {
-  chute = Number(prompt("Escolha um valor de 0 a 9: "));
+	chute = Number(prompt("Escolha um valor de 0 a 9: "));
 
-  if (isNaN(chute) || chute < 0 || chute > 9) {
-    alert("Por favor, insira um número válido entre 0 e 9.");
-    continue;
-  }
+	if (isNaN(chute) || chute < 0 || chute > 9) {
+		alert("Por favor, insira um número válido entre 0 e 9.");
+		continue;
+	}
 
-  if (chute === numeroSecreto) {
-    break;
-  } else if (chute > numeroSecreto) {
-    alert(`O número secreto é menor que ${chute}`);
-  } else {
-    alert(`O número secreto é maior que ${chute}`);
-  }
-  
-  tentativas++;
+	if (chute === numeroSecreto) {
+		break;
+	} else if (chute > numeroSecreto) {
+		alert(`O número secreto é menor que ${chute}`);
+	} else {
+		alert(`O número secreto é maior que ${chute}`);
+	}
+
+	tentativas++;
 }
 
 tentativas++; // Conta a tentativa do acerto
