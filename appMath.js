fec3e97d00b1262bb6eb35698cb8487791ac96ec
brasoares <henocsoares@hotmail.com.br>
2024-06-28 22:14:30 -0300

    Better Code!

--- appMath.js
+++ appMath.js
@@ -1,11 +1,18 @@
 alert("Seja bem-vindo(a) ao Jogo do Número Secreto!");
-let numeroSecreto = Math.floor(Math.random() * 10); // Gera um número aleatório entre 0 e 9
+
+const numeroSecreto = Math.floor(Math.random() * 10); // Gera um número aleatório entre 0 e 9
 console.log(numeroSecreto);
+
 let chute;
 let tentativas = 0;
 
 while (chute !== numeroSecreto) {
   chute = Number(prompt("Escolha um valor de 0 a 9: "));
+
+  if (isNaN(chute) || chute < 0 || chute > 9) {
+    alert("Por favor, insira um número válido entre 0 e 9.");
+    continue;
+  }
 
   if (chute === numeroSecreto) {
     break;
@@ -15,13 +22,9 @@ while (chute !== numeroSecreto) {
     alert(`O número secreto é maior que ${chute}`);
   }
   
-  tentativas++; 
+  tentativas++;
 }
 
 tentativas++; // Conta a tentativa do acerto
 
-if (tentativas > 1) {
-  alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-} else {
-  alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com 1 tentativa.`);
-}
+alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa(s).`);
