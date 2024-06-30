aa2c3eec54ed13da5de6ae7dafc7ca2ef060ea59
brasoares <henocsoares@hotmail.com.br>
2024-06-29 23:01:11 -0300

    Better Code!

--- apps.js
+++ apps.js
@@ -1,17 +1,33 @@
 alert("Seja bem-vindo(a) ao Jogo do Número Secreto!");
-let numeroSecreto = 7;
-console.log(numeroSecreto);
-let chute;
-let tentativas = 0;
 
-while(chute !== numeroSecreto){
-    chute = Number(prompt("Escolha um valor de 0 a 9: "));
-    tentativas++;
+const secretNumber = 7;
+let attempts = 0;
+let guess;
 
-    if (chute === numeroSecreto) {
-        let tentativasTexto = tentativas === 1 ? "vez" : "vezes";
-        alert(`Parabéns! Você acertou! O número secreto é o ${numeroSecreto} e você tentou acertá-lo com um total de ${tentativas} ${tentativasTexto}.`)
+function getGuess() {
+    return Number(prompt("Escolha um valor de 0 a 9: "));
+}
+
+function giveFeedback(guess, secretNumber) {
+    if (guess > secretNumber) {
+        alert(`O número secreto é menor que ${guess}`);
     } else {
-        alert(`O número secreto é ${chute > numeroSecreto ? "menor" : "maior"} que ${chute}`);
+        alert(`O número secreto é maior que ${guess}`);
+    }
+}
+
+function successMessage(secretNumber, attempts) {
+    const attemptsText = attempts === 1 ? "vez" : "vezes";
+    alert(`Parabéns! Você acertou! O número secreto é o ${secretNumber} e você tentou acertá-lo com um total de ${attempts} ${attemptsText}.`);
+}
+
+while (guess !== secretNumber) {
+    guess = getGuess();
+    attempts++;
+
+    if (guess === secretNumber) {
+        successMessage(secretNumber, attempts);
+    } else {
+        giveFeedback(guess, secretNumber);
     }
 }
